3cab3f5adefabd5714d2cad2a99cae9f0dd838d9
bear917 <bear917@users.noreply.github.com>
2015-02-11 13:53:58 +0000

    find price tag everywhere

--- coffee.py
+++ coffee.py
@@ -3,4 +3,9 @@ import urllib.request
 page = urllib.request.urlopen("http://beans-r-us.appspot.com/prices.html")
 text = page.read().decode("utf8")
 
-print(text)
+
+priceSearch = text.find(">$")
+priceStart = priceSearch + 2
+priceEnd = priceStart + 4
+price = text[priceStart:priceEnd]
+print(price)
