dec74ac2555c5f9bf02a51626acf2a107605c5cf
bear917 <bear917@users.noreply.github.com>
2015-02-11 14:05:38 +0000

    add comment for every statement

--- coffee.py
+++ coffee.py
@@ -3,9 +3,11 @@ import urllib.request
 page = urllib.request.urlopen("http://beans-r-us.appspot.com/prices.html")
 text = page.read().decode("utf8")
 
-
+# find the position of the price tag 
 priceSearch = text.find(">$")
+# after price tag, add offset 2
 priceStart = priceSearch + 2
+# price has 4 character 
 priceEnd = priceStart + 4
 price = text[priceStart:priceEnd]
 print(price)
